34f7910565c91cd3b8ba862ae9ae3ac4a59eeb26
SnehalDhoble <snehalsumitdhoble@gmail.com>
2019-12-25 12:42:56 -0800

    =Section 2 exercise solution and Section 3

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Dark Magic" },
+                new Movie { Id = 2, Name = "Toy Story"}
+            };
+        }
+
+    }
+}
